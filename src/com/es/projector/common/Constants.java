2352178e102048bf09237fc3d223441f8f99a12f
edonseki <edonseki@gmail.com>
2021-05-23 02:46:52 +0200

    Add RMI Registry to constants

--- src/com/es/projector/common/Constants.java
+++ src/com/es/projector/common/Constants.java
@@ -1,9 +1,9 @@
 package com.es.projector.common;
 
 public class Constants {
-    public static String APPLICATION_NAME = "Projector";
-    public static String APPLICATION_VERSION = "1.0";
-    public static String APPLICATION_AUTHOR = "Edon Sekiraqa";
-    public static int REFRESH_RATE = 30;
-
+    public final static String APPLICATION_NAME = "Projector";
+    public final static String APPLICATION_VERSION = "1.0";
+    public final static String APPLICATION_AUTHOR = "Edon Sekiraqa";
+    public final static int REFRESH_RATE = 30;
+    public final static String RMI_REGISTRY = "//%s:1099/ProjectorShareService%s";
 }
